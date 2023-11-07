5d24b1bab1ef0a170fb778620284fe73ac7163c6
LSF <lsf@arch.localdomain>
2023-11-06 23:02:10 -0300

    inserindo o website que foi feito

---
+++ website/view/login/main.js
@@ -0,0 +1,16 @@
+//alert("called");
+document.getElementById("submit-login-button").addEventListener("click",(params) =>{
+  const form = document.getElementById("login-form");
+
+  /*
+  if(form.classList.contains("animation-size")){
+    form.classList.remove("animation-size")
+    form.classList.add("animation-spinner")
+  } else {
+    form.classList.add("animation-size")
+    form.classList.remove("animation-spinner")
+  }
+  */
+})
+
+
