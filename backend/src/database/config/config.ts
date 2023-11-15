755ef569ae1e9b6f57e62cd9dc81398c42caaee2
felipe <felipe.raindo@gmail.com>
2023-11-15 06:02:14 -0300

    cria arquivo docker ignore

--- backend/src/database/config/config.ts
+++ backend/src/database/config/config.ts
@@ -1,9 +1,8 @@
 // SSOT
 import { dbConfig } from '../../SSOT/exporter';
 
-// Environment variables
-export const PORT = Number(process.env.PORT) || dbConfig.DB_DEFAULT_PORT;
-export const HOST = process.env.PATH || dbConfig.DB_DEFAULT_HOST;
+export const PORT = Number(process.env.PORT_DB) || dbConfig.DB_DEFAULT_PORT;
+export const HOST = process.env.HOST || dbConfig.DB_DEFAULT_HOST;
 export const DB_NAME = process.env.DB_NAME || dbConfig.DB_DEFAULT_NAME;
 export const DB_PASSWORD = process.env.DB_PASSWORD || dbConfig.DB_DEFAULT_PASSWORD;
 export const DB_USER = process.env.DB_USER || dbConfig.DB_DEFAULT_USER;
