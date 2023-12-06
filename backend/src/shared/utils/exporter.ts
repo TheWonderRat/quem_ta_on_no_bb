1b4e8a1a15d62c1740296d70d909a65d6f0efd40
lsf <lcrispindasilva@gmail.com>
2023-12-06 18:18:44 -0300

    exclui o diretorio 'src/shared/utils'

--- backend/src/shared/utils/exporter.ts
+++
@@ -1,4 +0,0 @@
-export { default as AppError } from './error/errorConstructor';
-export { default as TokenManager } from './jwt/tokenManager';
-export { default as authValidation } from './auth/authValidation';
-export { default as PasswordManager } from './bcrypt/passwordManager';
