de3294fda440f64671b25217b31132e479733776
lsf <lcrispindasilva@gmail.com>
2023-12-06 19:40:44 -0300

    exclui os tipos de servicos

--- backend/src/types/services/serviceTypes.ts
+++
@@ -1,8 +0,0 @@
-// Classes
-import AbstractService from '../../classes/service.class';
-import LoginService from '../../modules/service/LoginService';
-
-// Respositories
-import { RepositoryTypes } from '../repositories/repositoryTypes';
-
-export type ServiceTypes = LoginService | AbstractService<RepositoryTypes>;
