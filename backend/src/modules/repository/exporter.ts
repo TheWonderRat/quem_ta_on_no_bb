d60f4f30c1ad894711f6efd25e66d872dfee0e98
lsf <lcrispindasilva@gmail.com>
2023-12-06 20:48:10 -0300

    excluiu todos os repositorios que estavam sobrando do antigo ORM

--- backend/src/modules/repository/exporter.ts
+++
@@ -1,9 +0,0 @@
-// export { default as AprovadosRepository } from './AprovadoRepository';
-// export { default as CidadeRepository } from './CidadeRepository';
-// export { default as DiretoriaRepository } from './DiretoriaRepository';
-// export { default as ListaRepository } from './ListaRepository';
-// export { default as LotacaoRepository } from './LotacaoRepository';
-// export { default as LotacaoEmRepository } from './LotadoEmRepository';
-// export { default as SituacaoRepository } from './SituacaoRepository';
-// export { default as TipoListaRepository } from './TipoListaRepository';
-// export { default as TurmaRepository } from './TurmaRepository';
