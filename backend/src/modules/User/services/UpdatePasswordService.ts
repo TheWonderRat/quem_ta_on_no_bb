be70b66f80c071aa5f342c375158b6450cd83658
lsf <lcrispindasilva@gmail.com>
2023-12-09 16:00:39 -0300

    cria o servico de atualizar senha

--- backend/src/modules/User/services/UpdatePasswordService.ts
+++ backend/src/modules/User/services/UpdatePasswordService.ts
@@ -1,2 +1,12 @@
-export default class UpdatePasswordService{
+import AbstractService from "src/shared/service/AbstractService";
+import {  UpdatePasswordRequest, UpdatePasswordResponse } from "src/types/services/user";
+
+
+
+export default class UpdatePasswordService extends AbstractService<UpdatePasswordRequest, UpdatePasswordResponse>{
+
+  public async execute(parameters: UpdatePasswordRequest): Promise<UpdatePasswordResponse>{
+    return { email: "called on update password service" }
+      
+  }
 }
