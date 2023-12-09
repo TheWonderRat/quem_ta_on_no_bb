13146ddd46bb6ad6ece0caaf25dd436c9e76f24d
lsf <lcrispindasilva@gmail.com>
2023-12-09 16:15:15 -0300

    cria os tipos de servico para ativar conta e desativar conta

--- backend/src/types/services/user.ts
+++ backend/src/types/services/user.ts
@@ -1,2 +1,12 @@
-export type UserRequest = { email: string }
-export type UserResponse = { email: string }
+
+
+export type UpdatePasswordRequest = { email: string }
+export type UpdatePasswordResponse = { email: string }
+
+export type ActivateAccountRequest = { email: string }
+export type ActivateAccountResponse = { email: string }
+
+export type DeactivateAccountRequest = { email: string }
+export type DeactivateAccountResponse = { email: string }
+
+
