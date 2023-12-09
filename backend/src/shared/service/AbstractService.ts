30758901b6f7ed73c60866087d556c7a1b921d23
lsf <lcrispindasilva@gmail.com>
2023-12-09 15:58:55 -0300

    atualiza a classe AbstractService para obedever aos tipos ServiceRequestType e ServiceResponseTypes

--- backend/src/shared/service/AbstractService.ts
+++ backend/src/shared/service/AbstractService.ts
@@ -2,7 +2,7 @@
 import { ServiceRequestTypes, ServiceResponseTypes } from "src/types/services/service";
 
 
-export default abstract class AbstractService<ServiceRequestTypes, ServiceResponseTypes>{
+export default abstract class AbstractService< ServiceRequestType extends ServiceRequestTypes, ServiceResponseType extends ServiceResponseTypes >{
 
-  public abstract execute(parameters: ServiceRequestTypes): ServiceResponseTypes;
+  public abstract execute(parameters: ServiceRequestType): Promise<ServiceResponseType>;
 }
