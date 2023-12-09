887f718a3a437e5887f8ea042cfd6108bea46918
lsf <lcrispindasilva@gmail.com>
2023-12-09 15:48:58 -0300

    atualiza as mensagens do SSOT

--- backend/src/SSOT/error/messages.ts
+++ backend/src/SSOT/error/messages.ts
@@ -6,30 +6,13 @@ export enum AuthErrorMessage{
   InvalidToken = "O token encontrado e invalido",
 }
 
-export enum AuthErrorCode{
-  MissingToken = 400,
-  TokenOutOfDate = 401,
-  InvalidToken = 402
-}
-
 export enum DBErrorMessage{
-  MissingToken = "Miau",
-  TokenOutOfDate = "miau",
-  InvalidToken = "miau" 
-}
-export enum DBErrorCode{
   MissingToken = 400,
   TokenOutOfDate = 401,
   InvalidToken = 402
 }
 
 export enum GenericErrorMessage{
-  MissingToken = "message 1",
-  TokenOutOfDate = "message 2",
-  InvalidToken = "message 3" 
-}
-
-export enum GenericErrorCode{
   MissingToken = 400,
   TokenOutOfDate = 401,
   InvalidToken = 402
