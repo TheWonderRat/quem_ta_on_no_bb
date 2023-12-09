7a64537d881b8193664a9d68875babd84147da85
lsf <lcrispindasilva@gmail.com>
2023-12-09 15:52:56 -0300

    atualiza os tipos de roteador da classe router

--- backend/src/types/router/router.ts
+++ backend/src/types/router/router.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
-import LoginController from "src/modules/controller/LoginController";
 import ListController from "src/modules/List/controller/ListController";
+import SessionController from "src/modules/Session/controller/SessionController";
 import UserController from "src/modules/User/controller/UserController";
 
 export type RouterTypes = Router ;
-export type ControllerType = ListController | UserController | LoginController;
+export type ControllerTypes = ListController | UserController | SessionController;
