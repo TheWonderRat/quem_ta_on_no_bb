d3b1dc2499550de566c738be23e52fa39391d082
lsf <lcrispindasilva@gmail.com>
2023-12-09 15:59:46 -0300

    cria o servico de listar

--- backend/src/modules/List/services/ListService.ts
+++ backend/src/modules/List/services/ListService.ts
@@ -1,3 +1,11 @@
-export default class ListService{
+import AbstractService from "src/shared/service/AbstractService";
+import { ListRequest, ListResponse } from 'src/types/services/list'
+
+
+export default class ListService extends AbstractService<ListRequest, ListResponse>{
+
+  public async execute(parameters: ListRequest): Promise<ListResponse>{
+    return { email: "called on list request"}    
+  }
 
 }
