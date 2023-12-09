f91143663ef0e7d7e6eff5f6216b7b37cab4e4b5
lsf <lcrispindasilva@gmail.com>
2023-12-09 16:31:19 -0300

    insere variaveis de teste nos metodos de SessionController

--- backend/src/modules/Session/controller/SessionController.ts
+++ backend/src/modules/Session/controller/SessionController.ts
@@ -1,8 +1,14 @@
 
 import { Request, Response } from 'express'
+import CreateSessionService from '../services/CreateSessionService'
 
 export default class SessionController{
-  autenticar(request: Request, response: Response){
 
+  public async autenticar(request: Request, response: Response){
+
+    const sessionService = new CreateSessionService()
+    const {email} = request.params;
+    const result = await sessionService.execute({email});
+    return response.json(result)
   }
 }
