5beeb1a8e9eeb7feb3289fa66dca434c97a22aa1
lsf <lcrispindasilva@gmail.com>
2023-12-09 16:00:14 -0300

    cria a sessao de servico

--- backend/src/modules/Session/services/CreateSessionService.ts
+++ backend/src/modules/Session/services/CreateSessionService.ts
@@ -1,3 +1,10 @@
-export default class CreateSessionService{
+import AbstractService from "src/shared/service/AbstractService";
+import { CreateSessionRequest, CreateSessionResponse} from "src/types/services/session";
+
+export default class CreateSessionService extends AbstractService<CreateSessionRequest, CreateSessionResponse>{
+
+  public async execute(parameters: CreateSessionRequest): Promise<CreateSessionResponse>{
+     return { email: "called on create session request"} 
+  }
 
 }
