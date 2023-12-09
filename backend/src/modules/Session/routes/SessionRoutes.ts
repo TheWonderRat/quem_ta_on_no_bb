75dd86b7af561f212546314931461f0540f424ea
lsf <lcrispindasilva@gmail.com>
2023-12-09 16:04:02 -0300

    cria a rota de sessao no roteador de sesssao

--- backend/src/modules/Session/routes/SessionRoutes.ts
+++ backend/src/modules/Session/routes/SessionRoutes.ts
@@ -1,19 +1,35 @@
 import AbstractRouter from "src/shared/router/AbstractRouter";
 import { Router } from "express";
 import SessionController from "../controller/SessionController";
+ import { celebrate, Joi, Segments } from 'celebrate';
 
 
 
 
-export class SessionRouter extends AbstractRouter<Router, SessionController>{
+export default class SessionRouter extends AbstractRouter<Router, SessionController>{
+  constructor(){
+    //TODO: Incluir nome do arquivo de contantes
+    super("session",Router(),new SessionController())
+
+  }
 
   protected initRoutes(): void {
-      
+     this.createLoginRoute() 
   }
 
   
   protected createLoginRoute(): void{
-
+    this.router.post(
+      //TODO:: Incluir o nome da rota no arquivo de constantes
+   '/criar-sessao',
+   celebrate({
+     [Segments.BODY]: {
+       login: Joi.number().required(),
+       senha: Joi.string().required(),
+     },
+   }),
+    this.controller.autenticar
+  )
   }
 
 }
