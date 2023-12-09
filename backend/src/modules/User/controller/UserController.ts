7308d03af765b025ed123953cfc7997a06440418
lsf <lcrispindasilva@gmail.com>
2023-12-09 16:31:11 -0300

    insere variaveis de teste nos metodos de UserController

---
+++ backend/src/modules/User/controller/UserController.ts
@@ -0,0 +1,30 @@
+
+import { Request, Response } from 'express';
+import ActivateAccountService from '../services/ActivateAccountService';
+import DeactivateAccountService from '../services/DeactivateAccountService';
+import UpdatePasswordService from '../services/UpdatePasswordService';
+
+
+export default class UserController{
+  public async updatePassword(request: Request, response: Response){
+    const updateService = new UpdatePasswordService();
+    const {email} = request.params;
+    const result = await updateService.execute({email})
+
+    return response.json(result)
+  }
+  public async activateAccount(request: Request, response: Response){
+    const activateService = new ActivateAccountService()
+    const {email} = request.params;
+    const result = await activateService.execute({email});
+    return response.json(result)
+
+  }
+  public async deactivateAccount(request: Request, response: Response){
+    const deactivateService = new ActivateAccountService()
+    const {email} = request.params;
+    const result = await deactivateService.execute({email});
+    return response.json(result)
+
+  }
+}
