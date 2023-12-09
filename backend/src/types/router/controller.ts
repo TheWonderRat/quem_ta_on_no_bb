3dc3f18144cd5254e75a1e7c032c7f51766f5857
lsf <lcrispindasilva@gmail.com>
2023-12-09 15:52:36 -0300

    atualiza os tipos do controller da classe router

---
+++ backend/src/types/router/controller.ts
@@ -0,0 +1,6 @@
+import ListController from "src/modules/List/controller/ListController";
+import SessionController from "src/modules/Session/controller/SessionController";
+import UserController from "src/modules/User/controller/UserController";
+
+
+export type ControllerTypes = UserController | SessionController | ListController
