fdbd87abffec316689c0f4da9e0981e65c964f50
lsf <lcrispindasilva@gmail.com>
2023-12-10 14:47:29 -0300

    comenta alguns dos exports de tipos

--- backend/src/types/exporter.ts
+++ backend/src/types/exporter.ts
@@ -1,6 +1,9 @@
+export * as migrationsTypes from './migrations/exporter';
+
+/*
 export * as login from './login/login';
 export * as jwtTypes from './jwt/token';
 export * as errorTypes from './error/errors';
 export * as httpStatus from './status/httpStatus';
-export * as migrationsTypes from './migrations/exporter';
 export { ErrorMessages } from './error/messages';
+*/
