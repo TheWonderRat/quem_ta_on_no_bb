754c51156ae10513154d002f56325580552f3dd7
lsf <lcrispindasilva@gmail.com>
2023-12-10 15:22:33 -0300

    importa os novos parametros de routes corretament

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import listRouter from "src/modules/List/routes/ListRoutes";
-import sessionRouter from "src/modules/Session/routes/SessionRoutes";
-import userRouter from "src/modules/User/routes/UserRoutes";
-import { pathNames } from "src/SSOT/exporter";
+import listRouter from "../modules/List/routes/ListRoutes";
+import sessionRouter from "../modules/Session/routes/SessionRoutes";
+import userRouter from "../modules/User/routes/UserRoutes";
+import { pathNames } from "../SSOT/exporter";
 
 
 
