7e1a7de88edfeb172299e82bd8d9ea0e9ba5c179
lsf <lcrispindasilva@gmail.com>
2023-12-14 19:50:51 -0300

    exclui arquivos desnecessarios

--- backend/src/SSOT/migracoes/emComum.ts
+++
@@ -1,3 +0,0 @@
-export const UUID: string = 'uuid_generate_v4()';
-export const uuidQuery: string = 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp"';
-export const currentTimestamp: string = 'CURRENT_TIMESTAMP';
--- backend/src/SSOT/migracoes/exporter.ts
+++
@@ -1,3 +0,0 @@
-export * as emComum from './emComum';
-export * as entidades from './entidades';
-export * as atributos from './atributos';
