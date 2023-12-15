e233fd43b70cb61d0e15ef53b3247309f485b1ac
lsf <lcrispindasilva@gmail.com>
2023-12-15 12:52:36 -0300

    muda o nome do modulo de usuario para aprovado

--- backend/src/tipos/servicos/aprovado.ts
+++ backend/src/tipos/servicos/aprovado.ts
@@ -1,12 +1,18 @@
 
 
+//A validacao celebrate deve obedecer aos tipos
 export type RequisicaoParaAtualizarSenha = { email: string }
+//A validacao celebrate deve obedecer aos tipos
 export type RespostaParaAtualizarSenha= { email: string }
 
+//A validacao celebrate deve obedecer aos tipos
 export type RequisicaoParaAtivarConta= { email: string }
+//A validacao celebrate deve obedecer aos tipos
 export type RespostaParaAtivarConta = { email: string }
 
+//A validacao celebrate deve obedecer aos tipos
 export type RequisicaoParaDesativarConta= { email: string }
+//A validacao celebrate deve obedecer aos tipos
 export type RespostaParaDesativarConta= { email: string }
 
 
