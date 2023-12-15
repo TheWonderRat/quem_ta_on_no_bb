592a10778aee538b00d3549f39fb9f4bb62a2ff9
lsf <lcrispindasilva@gmail.com>
2023-12-15 12:24:53 -0300

    cria o exporter de erros

--- backend/src/compartilhados/erros/exporter.ts
+++ backend/src/compartilhados/erros/exporter.ts
@@ -1,1 +1,13 @@
+//classe abstrata
 export { default as AppError } from './AppError';
+//erros de autenticacao
+export { default as TokenExpirou } from './autenticacao/TokenExpirou';
+export { default as TokenInvalido } from './autenticacao/TokenInvalido';
+export { default as TokenNaoEncontrado } from './autenticacao/TokenNaoEncontrado';
+//erros de autorizacao
+export { default as SenhaIncompativel } from './autorizacao/SenhaIncompativel';
+export { default as UsuarioNaoExiste } from './autorizacao/UsuarioNaoExiste';
+//erros da base de dados 
+export { default as ErroDaBaseDeDados } from './base_de_dados/ErroDaBaseDeDados';
+//erros desconhecidos 
+export { default as ErroNoServidor } from './desconhecido/ErroNoServidor';
