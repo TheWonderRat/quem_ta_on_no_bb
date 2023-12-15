6cd0c4bc807e968ace0bd4e7b22fa782b91b29ee
lsf <lcrispindasilva@gmail.com>
2023-12-15 12:15:31 -0300

    Cria o repositorio de Cidade

---
+++ backend/src/database/ORM/repositorio/CidadeRepo.ts
@@ -0,0 +1,26 @@
+  //libraries
+ import { Repository } from 'typeorm';
+
+  //ORM
+import dataSource from '../../config';
+  //entity
+import { Cidade } from '../modelo/exporter'
+//relacionamentos
+import { atributos, entidades, relacionamentos } from '../../../SSOT/base_de_dados/exporter';
+
+ class CidadeRepo extends Repository<Cidade> {
+  public async cadastrarCidade(nome: string, estado: string): Promise<void>{
+
+    const cidade = this.criarCidade(nome, estado);
+    await this.manager.save(cidade);
+  }
+
+  public criarCidade(nome: string,estado: string): Cidade{
+      const cidade = this.manager.create(Cidade);
+      cidade.nome = nome;
+      cidade.estado = estado;
+
+    return cidade;
+  }
+}
+ export default new CidadeRepo(Cidade, dataSource.manager);
