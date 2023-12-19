03e18b75709104ea35766da89611696d885ad51d
lsf <lcrispindasilva@gmail.com>
2023-12-19 16:15:14 -0300

    alteracoes para conformar o servidor com a nova estrutura de diretorios

--- backend/src/servidor/servidor.ts
+++ backend/src/servidor/servidor.ts
@@ -6,20 +6,16 @@ import appRoutes from '../rotas/rotas';
 import cors from 'cors';
 import dataSource from '../database/config';
 
-import AppError from '../compartilhados/erros/AppError';
-import { Request, Response } from 'express';
-import { codigoErroHTTP, mensagemErroHTTP} from '../SSOT/exporter';
-
 // SSOT
 import { serverConfig } from '../SSOT/exporter';
-import { errorMiddleWare } from '../funcoes/exporter';
+import { GerenciadorDeErros } from '../compartilhados/utilitarios/exporter';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(appRoutes);
 app.use(errors())
-app.use(errorMiddleWare)
+app.use(GerenciadorDeErros.gerenciar)
 
 
 
