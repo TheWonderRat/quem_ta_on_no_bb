5eded51c531a921c625ebb7203b927b27d9adb45
lsf <lcrispindasilva@gmail.com>
2023-12-18 23:21:36 -0300

    altera o servidor para aceitar o middleware de erros

--- backend/src/servidor/servidor.ts
+++ backend/src/servidor/servidor.ts
@@ -1,19 +1,26 @@
 // types
 import 'reflect-metadata'
-import express from 'express';
+import express, { NextFunction } from 'express';
 import { errors } from 'celebrate';
 import appRoutes from '../rotas/rotas';
 import cors from 'cors';
 import dataSource from '../database/config';
 
+import AppError from '../compartilhados/erros/AppError';
+import { Request, Response } from 'express';
+import { codigoErroHTTP, mensagemErroHTTP} from '../SSOT/exporter';
+
 // SSOT
 import { serverConfig } from '../SSOT/exporter';
+import { errorMiddleWare } from '../funcoes/exporter';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(appRoutes);
 app.use(errors())
+app.use(errorMiddleWare)
+
 
 
   // public methods
