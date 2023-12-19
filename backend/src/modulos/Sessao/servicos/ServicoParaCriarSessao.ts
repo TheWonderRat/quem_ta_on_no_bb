405b5589f7f243b2de45876f7d62f92ed2a2c6b4
lsf <lcrispindasilva@gmail.com>
2023-12-19 16:14:20 -0300

    alteracoes para conformar o modulo de sessao com a nova estrutura de diretorios

--- backend/src/modulos/Sessao/servicos/ServicoParaCriarSessao.ts
+++ backend/src/modulos/Sessao/servicos/ServicoParaCriarSessao.ts
@@ -2,7 +2,7 @@ import AbstractService from "../../../compartilhados/servico/ServicoAbstrato";
 import { RequisicaoParaCriarSessao, RespostaParaCriarSessao} from "../../../tipos/servicos/sessao";
 import { AprovadoRepo } from "../../../database/ORM/repositorio/exporter";
 import { UsuarioNaoExiste, SenhaIncompativel,ContaEstaInativa, AppError } from "../../../compartilhados/erros/exporter";
-import { senhaUtils, tokenUtils } from "../../../funcoes/exporter";
+import { GerenciadorDeSenha, GerenciadorDeAutenticacao } from "../../../compartilhados/utilitarios/exporter";
 
 export default class ServicoParaCriarSessao extends AbstractService<RequisicaoParaCriarSessao,RespostaParaCriarSessao>{
 
@@ -17,7 +17,7 @@ export default class ServicoParaCriarSessao extends AbstractService<RequisicaoPa
       return new UsuarioNaoExiste()
     }
 
-    const senhaConfere = await senhaUtils.compararSenha(aprovado.senha,parameters.senha )
+    const senhaConfere = await GerenciadorDeSenha.compararSenha(aprovado.senha,parameters.senha )
 
 
     //checa se a conta do aprovado esta ativa
@@ -31,7 +31,7 @@ export default class ServicoParaCriarSessao extends AbstractService<RequisicaoPa
       return new SenhaIncompativel()
     }
 
-    const token = tokenUtils.gerarToken(login);
+    const token = GerenciadorDeAutenticacao.gerarToken(login.toString());
     
     //TODO:: retorna um token ao usuario
 
