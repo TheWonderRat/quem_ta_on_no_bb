3d4b484779bb91ff0486e5595a3e8cdae2538fb9
lsf <lcrispindasilva@gmail.com>
2023-12-21 13:03:03 -0300

    cria o tipo de resposta para o servico de listar

--- backend/src/tipos/servicos/ranking.ts
+++ backend/src/tipos/servicos/ranking.ts
@@ -7,4 +7,16 @@ export type RequisicaoParaListarORanking = {
   turma?: number,
   situacao?: string,
 };
-export type RespostaParaListarORanking = { mensagem: string, aprovados: any }; //  TODO:: criar os tipos de resposta para aprovados//
+export type RespostaParaListarORanking = { mensagem: string, aprovados: any}; //  TODO:: criar os tipos de resposta para aprovados//
+/*
+type RespostaRankingBase = {
+  ppp: boolean,
+  pcd: boolean,
+  posicao: number,
+  ranking: string,
+  turma?: number,
+  situacao?: string,
+  cidade?: string,
+  diretoria?: string
+}
+*/
