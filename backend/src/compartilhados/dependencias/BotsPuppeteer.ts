cf687e5dc79ee9aee6f549c18a3181d6254efe61
lsf <lcrispindasilva@gmail.com>
2023-12-21 13:04:53 -0300

    cria a classe base para gerenciamento de bots

---
+++ backend/src/compartilhados/dependencias/BotsPuppeteer.ts
@@ -0,0 +1,21 @@
+export default class PuppeteerWrapper{
+  //  TODO:: criar propriedade com uma instancia do puppeteer
+  constructor(){
+  }
+
+  //  TODO:: Criar tipo para os tipos de item admitidos. classe, id, etc...
+  public clicarEm(tag: string){
+  }
+
+  //  TODO:: Criar tipo para os tipos de item admitidos. classe, id, etc...
+  public moverOCursorAte(tag: string){
+  }
+
+  //  TODO:: Criar tipo para os tipos de item admitidos. classe, id, etc...
+  public digitar(expressao: string,item: string){
+  }
+
+  public resolverCAPTCHA(){
+
+  }
+}
