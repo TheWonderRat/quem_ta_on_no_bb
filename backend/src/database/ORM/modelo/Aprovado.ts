375c466169a099b20ad629a2940e12dc0615d3a3
lsf <lcrispindasilva@gmail.com>
2024-01-06 12:07:22 -0300

    insere o atributo de inscricao na entidade Aprovado

--- backend/src/database/ORM/modelo/Aprovado.ts
+++ backend/src/database/ORM/modelo/Aprovado.ts
@@ -1,6 +1,27 @@
-import { Contato, LotadoEm, Situacao, Turma, Ranking } from '../modelo/exporter';
-import { Entity, Column, PrimaryColumn, ManyToOne, JoinColumn, OneToOne, OneToMany, } from 'typeorm'
-import { atributos, entidades } from '../../../SSOT/base_de_dados//exporter';
+import { 
+  Entity, 
+  Column, 
+  PrimaryColumn, 
+  ManyToOne, 
+  JoinColumn, 
+  OneToOne, 
+  OneToMany, 
+  CreateDateColumn, 
+  UpdateDateColumn, 
+  DeleteDateColumn, 
+} from 'typeorm'
+import { 
+  atributos, 
+  entidades, 
+  valoresPadrao
+} from '../../../SSOT/base_de_dados//exporter';
+import { 
+  Contato, 
+  LotadoEm, 
+  Situacao, 
+  Turma,
+  Ranking 
+} from '../modelo/exporter';
 
 //TODO:: inserir no arquivo de constantes
 @Entity(entidades.Aprovado)
@@ -8,6 +29,9 @@ export default class Aprovado{
 
   @PrimaryColumn({name: atributos.Aprovado.PosicaoAmpla , type: 'smallint'})
   posicao: number;
+
+  @Column({name: atributos.Aprovado.Inscricao,unique: true, type: 'bigint'})
+  inscricao: number;
 
   @Column({name: atributos.Aprovado.Nome})
   nome: string;
@@ -29,6 +53,16 @@ export default class Aprovado{
 
   @Column()
   ativado: boolean
+
+  @CreateDateColumn()
+  createdAt: Date
+
+  @UpdateDateColumn()
+  updatedAt: Date
+
+  @DeleteDateColumn()
+  deletedAt: Date
+
 
   /*-------------------------------joins------------------------------*/
   //as joins devem ter exatamente o mesmo nome das contantes de SSOT/base_de_dados/relacionamentos
@@ -52,4 +86,17 @@ export default class Aprovado{
 
   @OneToMany(() => Ranking, (ranking) => ranking.aprovadoVinculado )
   rankingsDoAprovado: Ranking[]
+
+  //-------------------------------------getters/setters
+  public getNome(): string{
+    return this.nome;
+  }
+
+  public setNome(nome: string){
+    this.nome = nome;
+  }
+
+  public setSituacao(situacao: valoresPadrao.Situacao){
+    this.situacao = situacao;
+  }
 }
