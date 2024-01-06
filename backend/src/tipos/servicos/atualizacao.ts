29e34333693ad22ad843d0ff8614bd874f1196e5
lsf <lcrispindasilva@gmail.com>
2024-01-06 12:15:00 -0300

    insere os tipos de requisicao dos servicos de atualizacao

---
+++ backend/src/tipos/servicos/atualizacao.ts
@@ -0,0 +1,5 @@
+export type RequisicaoAtualizarTodos = { captcha: string, userAgent?: string };
+export type RespostaAtualizarTodos = { mensagem: string };
+
+export type RequisicaoAtualizarEmMudanca = { captcha: string, userAgent?: string };
+export type RespostaAtualizarEmMudanca = { mensagem: string };
