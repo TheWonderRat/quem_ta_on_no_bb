53e8ae4fc981a758f756d035eabd3cff555b4843
lsf <lcrispindasilva@gmail.com>
2024-01-06 12:14:32 -0300

    insere a rota de atualizacao no roteador principal

--- backend/src/rotas/rotas.ts
+++ backend/src/rotas/rotas.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import rotasDeRanking from '../modulos/Ranking/rotas/RotasDeRanking';
 import rotasDeSessao from '../modulos/Sessao/rotas/RotasDeSessao';
 import rotasDeUsuario from '../modulos/Aprovado/rotas/RotasDeUsuario';
+import rotasDeAtualizacao from '../modulos/Atualizacao/rotas/RotasDeAtualizacao';
 import { caminhos } from '../SSOT/exporter';
 
 const rotasDoApp = Router();
@@ -9,5 +10,6 @@ const rotasDoApp = Router();
 rotasDoApp.use(caminhos.ranking, rotasDeRanking);
 rotasDoApp.use(caminhos.sessao, rotasDeSessao);
 rotasDoApp.use(caminhos.usuario, rotasDeUsuario);
+rotasDoApp.use(caminhos.atualizacao, rotasDeAtualizacao);
 
 export default rotasDoApp;
