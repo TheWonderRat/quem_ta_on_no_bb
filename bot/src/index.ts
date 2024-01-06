0687e1ad4c9ce6365ccdfeee977d45a0b487900d
lsf <lcrispindasilva@gmail.com>
2024-01-06 12:20:47 -0300

    arquivo principal do container de bots

---
+++ bot/src/index.ts
@@ -0,0 +1,7 @@
+//  usuario manda a requisicao para o bot, bot reencaminha para o servidor,
+//  servidor retorna um erro ou um ok com os usuarios que devem ser atualizados
+function helloWorld(){
+  console.log("called properly!") 
+}
+
+helloWorld();
