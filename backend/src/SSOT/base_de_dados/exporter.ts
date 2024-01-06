cd5eeb6c7be1cefcd0fc22715f8a3055bd5ee0a9
lsf <lcrispindasilva@gmail.com>
2024-01-06 11:54:36 -0300

    ajusta o exporter do SSOT

--- backend/src/SSOT/base_de_dados/exporter.ts
+++ backend/src/SSOT/base_de_dados/exporter.ts
@@ -1,3 +1,4 @@
 export * as entidades from './entidades';
 export * as atributos from './atributos';
 export * as relacionamentos from './relacionamentos';
+export * as valoresPadrao from './valoresPadrao'
