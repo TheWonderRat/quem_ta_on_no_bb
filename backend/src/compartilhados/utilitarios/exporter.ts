36636a66ec97c0ebcf299925582297b66600b9d0
lsf <lcrispindasilva@gmail.com>
2024-01-06 12:07:01 -0300

    atualiza o exporter de utilitarios para exportar o gerenciador de bots

--- backend/src/compartilhados/utilitarios/exporter.ts
+++ backend/src/compartilhados/utilitarios/exporter.ts
@@ -1,3 +1,5 @@
 export { default as GerenciadorDeErros } from './GerenciadorDeErros';
 export { default as GerenciadorDeSenha } from './GerenciadorDeSenha';
 export { default as GerenciadorDeAutenticacao} from './GerenciadorDeAutenticacao';
+export { default as GerenciadorDeBots } from './GerenciadorDeBots';
+export { default as GerenciadorDeAtualizacoes } from './GerenciadorDeAtualizacoes'
