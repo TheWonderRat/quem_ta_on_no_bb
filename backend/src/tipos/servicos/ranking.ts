b78b284e0a6770cb53b538a1123c943fe91964cf
lsf <lcrispindasilva@gmail.com>
2024-01-06 12:16:24 -0300

    altera os tipos de requisicao para os servicos de ranking

--- backend/src/tipos/servicos/ranking.ts
+++ backend/src/tipos/servicos/ranking.ts
@@ -8,15 +8,3 @@ export type RequisicaoParaListarORanking = {
   situacao?: string,
 };
 export type RespostaParaListarORanking = { mensagem: string, aprovados: any}; //  TODO:: criar os tipos de resposta para aprovados//
-/*
-type RespostaRankingBase = {
-  ppp: boolean,
-  pcd: boolean,
-  posicao: number,
-  ranking: string,
-  turma?: number,
-  situacao?: string,
-  cidade?: string,
-  diretoria?: string
-}
-*/
