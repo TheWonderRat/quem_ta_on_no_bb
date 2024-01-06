6942d94fc8f0059b9be0f7266f418aeae99277e8
lsf <lcrispindasilva@gmail.com>
2024-01-06 12:04:51 -0300

    insere o tratamento de erros do gerenciador de bots

--- backend/src/compartilhados/utilitarios/GerenciadorDeBots.ts
+++ backend/src/compartilhados/utilitarios/GerenciadorDeBots.ts
@@ -1,10 +1,21 @@
-export default class GerenciadorDeBots{
-  constructor(){
-  }
+import { BotsPuppeteer } from "../dependencias/exporter";
+import { Aprovado } from '../../database/ORM/modelo/exporter'
+//import CapsulaAtualizarAprovado from "./GerenciadorDeAtualizacoes";
+
+
+class GerenciadorDeBots{
 
   //  atualiza as listas da base de dados
-  public atualizarListas(){
-
+  //  ainda nao sei se devo user websockets nesse caso
+  //  por enquanto nao
+  public async atualizarListas(
+    captcha: string,
+    aprovados: Aprovado[],
+    userAgent?: string
+  ): Promise<void>{
+    await BotsPuppeteer.atualizarAprovados(captcha,aprovados,userAgent) ;
   }
 
 }
+
+export default new GerenciadorDeBots();
